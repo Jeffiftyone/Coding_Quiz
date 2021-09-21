0e22b086f2eb563aea94bca6c358724c89ba74b1
Jeffrey.Beh <jeffrey.beh@hotmail.com>
2021-09-21 00:30:43 -0400

    skeleton created

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+//Coding quiz
+//Put questions as objects
+let question ={
+    text: "Which of the following should be used to save an assigned value?",
+    answer : "variable",
+    option1 : "variable",
+    option2 : "class",
+    option3 : "type",
+    option4 : "attribute"
+};
+
+//once the start button is pressed
+function startGame(){
+
+}
+
+//Timer begins
+function startTimer(){
+
+}
+//main page switched to questions
+function questionPage(){
+
+}
+
+//if user selects an answer, they proceeed to the next question
+
+//if the answer is correct their score is increased
+
+//if the answer is incorrect, the time is lowered by 10 seconds
+
+//when timer hits 0, game is over, score is displayed
+
+//user can enter their initials to highscore (saved locally)
+
+//user can wipe scores too by pressing 'clear scores' or go back to the first screen
