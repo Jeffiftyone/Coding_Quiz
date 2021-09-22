eff077c733e8842e5b4309e630d6f620c0090cb9
Jeffrey.Beh <jeffrey.beh@hotmail.com>
2021-09-21 23:32:16 -0400

    end of game logic added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,14 @@
 //Coding quiz
 let timerElement=document.querySelector(".timer-count");
 let mainText=document.querySelector(".main-text");
-let startButton=document.querySelector(".start-btn");
+let startButton=document.getElementById("start-btn");
+let scorePanel=document.querySelector(".score-screen");
+let endScreen=document.querySelector(".end-screen");
+let scoreList=document.getElementById("scores");
 let timerCount;
+let player="";
+let score=5;
+let prevScore=0;
 //Put questions as objects
 let question = [{
     text: "Which of the following should be used to save an assigned value?",
@@ -51,13 +57,10 @@ let question = [{
 //once the start button is pressed
 function startGame(){
    timerCount=10;
+   //hide start button
+   startButton.style.display="none";
       startTimer();
-
-    
-
-
 }
-
 //Timer begins
 function startTimer(){
     //set timer
@@ -66,14 +69,12 @@ function startTimer(){
         timerElement.textContent=timerCount;
         if (timerCount===0){
             clearInterval(timer); //clears the interval
-            scoreScreen();
+            endGame();
         }
     },1000);
-
 }
 //main page switched to questions
 function questionPage(question){
-
 }
 
 //if user selects an answer, they proceeed to the next question
@@ -82,25 +83,57 @@ function questionPage(question){
 //if the answer is incorrect, the time is lowered by 10 seconds
 
 //when timer hits 0, game is over, score is displayed
-function scoreScreen(){
-    mainText.textContent="Enter your initials";
-    let btn=document.createElement("button");
-    btn.innerHTML="Submit";
-    btn.type="submit";
-    btn.name="formBtn";
-    btn.onclick = function () {
-        alert("Button is clicked");
-      };
-    document.body.main.section.appendChild(btn);
+function endGame(){
+    //user can enter their initials to highscore (saved locally)
+    console.log("End Screen enabled");
+    mainText.textContent="Enter your initials: ";
+    //create form
+    let initialInput=document.createElement("input");
+    initialInput.setAttribute("type","text");
+    initialInput.setAttribute("name", "initials");
+    initialInput.setAttribute("placeholder", "Enter your initials here");
+    initialInput.setAttribute("maxlength","3");
 
+    // create a submit button
+    var submitButton = document.createElement("input");
+    submitButton.setAttribute("type", "submit");
+    submitButton.setAttribute("value", "Submit");
+
+    //button is clicked
+    submitButton.addEventListener("click",function(){
+        //add to local storage
+    localStorage.setItem("score",score);
+    localStorage.setItem("player",initialInput.value);
+    
+    console.log("submit button pressed");
+
+    //add new score to score list
+    let newScore = document.createElement("li");
+    let scoreInfo = document.createTextNode(initialInput.value+" : " +score);
+    newScore.appendChild(scoreInfo);
+    //if new score is higher, add to top of list
+        if (prevScore<score){
+            scoreList.prepend(newScore);
+        }
+        else{
+            scoreList.appendChild(newScore);
+        }
+    //scoreScreen(); 
+    
+    });
+    //create buttons
+    endScreen.appendChild(initialInput);
+    endScreen.appendChild(submitButton);
+}
+
+function scoreScreen(){
 
 }
-//user can enter their initials to highscore (saved locally)
 
 //user can wipe scores too by pressing 'clear scores' or go back to the first screen
 
 startButton.onclick = function(){
-    console.log("button clicked")
+    console.log("Start button clicked")
     startGame();
 };
 
